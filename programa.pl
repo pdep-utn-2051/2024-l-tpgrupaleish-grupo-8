62e357bc84720e92509282a974bf7b6c5caf2d71
Agustin Monzon Brito <mmonznbrito@frba.utn.edu.ar>
2024-08-06 00:09:26 -0300

    Resolucion de puntos faltantes y correcciones.

--- programa.pl
+++ programa.pl
@@ -43,6 +43,7 @@ alcanceGlobal(Tecnologia):- tecnologia(Tecnologia), forall(personajes(Persona), 
 % punto 5
 tecnologiasCivilizacion(Civilizacion,ListaTecnologias):- findall(Tecnologia,(juegaCon(Persona,Civilizacion), desarrollo(Persona,Tecnologia)),ListaTecnologias).
 
+%listaContenidaEn(subLista, ListaCompleta)
 listaContenidaEn([],_).
 listaContenidaEn([X|Xs],Lista):- member(X,Lista), listaContenidaEn(Xs,Lista).
 
@@ -51,6 +52,7 @@ civilizacionLider(Civilizacion):- tecnologiasCivilizacion(Civilizacion,Tecnologi
 %%%Unidades%%%
 
 % punto 6
+% unidades(Jugador, Unidades)
 unidades(ana, jinetes(caballo)).
 unidades(ana, piquero(escudo, 1)).
 unidades(ana, piquero(sinEscudo, 2)).
@@ -62,6 +64,8 @@ unidades(carola, piquero(sinEscudo, 3)).
 unidades(carola, piquero(escudo, 2)).
 
 % punto 7
+%cantVida(Unidad, Vida)
+cantVida(campeon(Vida), VidaCampeon):- Vida >= 1, Vida =< 100, VidaCampeon is Vida.
 cantVida(jinetes(camello), 80).
 cantVida(jinetes(caballo), 90).
 cantVida(piquero(sinEscudo, 1), 50).
@@ -72,3 +76,29 @@ cantVida(piquero(escudo, Nivel), VidaPiquero):- cantVida(piquero(sinEscudo, Nive
 unidadConMasVida(Jugador, UnidadConMasVida) :-generaListaUnidades(Jugador, ListaUnidades), max_member(_-UnidadConMasVida, ListaUnidades).
 
 generaListaUnidades(Jugador, ListaUnidades):-findall(Vida-Unidad, (unidades(Jugador, Unidad), cantVida(Unidad, Vida)), ListaUnidades).
+
+% punto 8
+%ventajas("Unidad que gana", "Unidad que pierde")
+ventajas(jinetes(_), campeon(_)).
+ventajas(campeon(_), piquero(_,_)).
+ventajas(piquero(_,_), jinetes(_)).
+ventajas(jinetes(camello),jinetes(caballo)).
+%leGana("Unidad que gana", "Unidad que pierde")
+leGana(Unidad, OtraUnidad):- not(ventajas(Unidad,OtraUnidad)), cantVida(Unidad,Vida1), cantVida(OtraUnidad,Vida2), Vida1 > Vida2.
+leGana(Unidad, OtraUnidad):- ventajas(Unidad,OtraUnidad).
+
+% punto 9 
+contarPiqueros(Jugador, ConEscudo, SinEscudo) :-
+    findall(piquero(escudo, _), unidades(Jugador, piquero(escudo, _)), PiquerosConEscudo),
+    findall(piquero(sinEscudo, _), unidades(Jugador, piquero(sinEscudo, _)), PiquerosSinEscudo),
+    length(PiquerosConEscudo, ConEscudo),
+    length(PiquerosSinEscudo, SinEscudo).
+
+jugadorSobreviveAsedio(Jugador) :-
+    contarPiqueros(Jugador, ConEscudo, SinEscudo),
+    ConEscudo > SinEscudo.
+
+% punto 10.
+/*puedeDesarrollarTecnologia(Jugador,Tecnologia):- obtieneDependencias(Tecnologia,ListaDeDependiencias), jugadorDesarrollo(Jugador,ListaDeDesarrollos), listaContenidaEn(ListaDeDesarrollos,ListaDeDependencias).
+
+*/
